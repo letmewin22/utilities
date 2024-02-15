981de3733455b983548951c40fc74da82cc4d71f
letmewin22 <lkosteckiy5@gmail.com>
2024-02-15 16:09:26 +0200

    feat: build raf updates

--- dist/raf/raf.d.ts
+++ dist/raf/raf.d.ts
@@ -1,10 +1,10 @@
-declare type TFunc = () => void;
+declare type TFunc = (...args: any) => void;
 export declare class RAF {
     cbArray: Array<null | TFunc>;
     constructor();
     on(cb: TFunc): void;
     off(cb: TFunc): void;
-    animation(): void;
+    animation(delta: number): void;
 }
 export declare const raf: {
     on: (cb: TFunc) => void;
